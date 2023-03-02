26b519d276fd8021e5de3d4992078c3ec3b2b3a2
Joshua Poveda <joshuapoveda9@gmail.com>
2023-03-02 13:31:46 -0500

    new files

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('hello world!')
+// for(let i = 10;i > 0; i--){
+//     console.log(i)
+// }
+////////
+// x = [];
+// for (y = 0; y < 1; y++) {
+//   for (let i = 1; i < 10; i++) {
+//     if (i % 2 != 0) {
+//       x += i;
+//     }
+//   }
+//   for (let i = 1; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//       x += i;
+//     }
+//   }
+// }
+
+// console.log(x);
+////////
+for(i = 1;i < 60;i++){
+    if(i % 3 ==0){
+        console.log(i)
+    }
+}
