ed2a624481e158b1f0f6fb59ee9e90fb2dc12e93
Joshua Poveda <joshuapoveda9@gmail.com>
2023-03-01 23:39:52 -0500

    test

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-// function century(year) {
-//   if(year % 10 === 0){
-//     return Math.ceil(year / 100)
-//   } else{
-//     return Math.trunc((year+100)/100)
-// }
-// }
-
-//or 
-
-// const century = year => Math.ceil(year/100)
-
-
-// console.log(century(1907))
+console.log('hello world!')
